34b597d382de7c7118e9147fe2567b3945fcbee5
John Dohoney Jr <johndohoneyjr@Blackhawk.fios-router.home>
2018-12-03 17:01:15 -0800

    Added Data resource

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ module "compute" {
     instance_count = "${var.instance_count}"
     key_name = "${var.key_name}"
     instance_type = "${var.server_instance_type}"
-    subnets = "${terraform_remote_state.networking.public_subnets}"
-    security_group = "${terraform_remote_state.networking.public_sg}"
-    subnet_ips = "${terraform_remote_state.networking.subnet_ips}"
+    subnets = "${data.terraform_remote_state.networking.public_subnets}"
+    security_group = "${data.terraform_remote_state.networking.public_sg}"
+    subnet_ips = "${data.terraform_remote_state.networking.subnet_ips}"
 }
